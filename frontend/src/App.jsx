3143648bdcc700d8b8b97c9fa3ca46431b1cd588
anuj1g <anuj26it003@satiengg.in>
2025-08-02 14:45:03 +0530

    Added pages and items in mens women kids section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import LoginSignup from "./Pages/LoginSignup"
 import Shop from "./Pages/Shop"
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
+import Footer from './Components/Footer/Footer'
+import men_banner from "./assets/banner.jpg"
+import women_banner from "./assets/women_banner.avif"
+import kids_banner from "./assets/kids_banner.jpg"
+
 
 
 
@@ -17,9 +22,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Shop/>}/>
-        <Route path="/mens" element={<ShopCategory category="mens"/>}/>
-        <Route path="/womens" element={<ShopCategory category="womens"/>}/>
-        <Route path="/kids" element={<ShopCategory category="kids"/>}/>
+        <Route path="/mens" element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path="/womens" element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}/>
         <Route path=":productId" element={<Product/>}/>
         <Route path="/cart" element={<Cart/>}/>
@@ -29,6 +34,8 @@ function App() {
         
         
       </Routes>
+      <Footer/>
+
       </BrowserRouter>
 
       
