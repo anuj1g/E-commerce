854eb97614b19952be12cbee72a96c349438d039
anuj1g <anuj26it003@satiengg.in>
2025-08-06 23:35:23 +0530

    more updates regarding media queries

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <div className='nav-logo'>
-        <img src={logo} alt=" " height="50px"/>
+        <img src={logo} alt="Shopify Logo" height="50px"/>
         <p>Shopify</p>
       </div>
       <ul className='nav-menu'>
@@ -31,7 +31,7 @@ const Navbar = () => {
       </ul>
       <div className='nav-login-cart'>
         <Link to="/login"><button>Login</button></Link>
-        <Link to="/cart"><img src={cart_icon} alt="" height="40px"/></Link>
+        <Link to="/cart"><img src={cart_icon} alt="Shopping Cart" height="40px"/></Link>
         <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,27 +1,78 @@
-import React from 'react'
-import "../CSS/LoginSignup.css"
+import React, { useState } from 'react';
+import "../CSS/LoginSignup.css";
+import { Link } from 'react-router-dom'; // Link ko import kiya hai
+
 const LoginSignup = () => {
-  return (
-    <div className='loginsignup'>
-      <div className="loginsignup-container">
-        <h1>Sign Up</h1>
-        <div className='loginsignup-fields'>
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Email Address'/>
-          <input type='password' placeholder='Password'/>
-          <button>Continue</button>
+    // Nai state jo form ke data ko manage karegi
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+    const [isLogin, setIsLogin] = useState(false); // Login state add kiya
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Submitted:", formData);
+    };
+
+    return (
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>{isLogin ? "Login" : "Sign Up"}</h1> {/* Heading ko dynamic kiya */}
+                <form onSubmit={handleSubmit}>
+                    <div className='loginsignup-fields'>
+                        {!isLogin && ( // Agar login nahi hai, to name field dikhega
+                            <input 
+                                type='text' 
+                                placeholder='Your Name' 
+                                name='name' 
+                                value={formData.name} 
+                                onChange={handleChange} 
+                                required 
+                            />
+                        )}
+                        <input 
+                            type='email' 
+                            placeholder='Email Address' 
+                            name='email' 
+                            value={formData.email} 
+                            onChange={handleChange} 
+                            required
+                        />
+                        <input 
+                            type='password' 
+                            placeholder='Password' 
+                            name='password' 
+                            value={formData.password} 
+                            onChange={handleChange} 
+                            required
+                        />
+                        <button type="submit">{isLogin ? "Login" : "Continue"}</button>
+                    </div>
+                </form>
+                
+                <p className="loginsignup-login">
+                    {isLogin ? (
+                        <>Don't have an account? <span onClick={() => setIsLogin(false)}>Sign Up here</span></>
+                    ) : (
+                        <>Already have an account? <span onClick={() => setIsLogin(true)}>Login here</span></>
+                    )}
+                </p>
+                
+                {!isLogin && ( // Agar login nahi hai, to agree checkbox dikhega
+                    <div className='loginsignup-agree'>
+                        <input type='checkbox' name='' id='' required/>
+                        <p>By continuing, I agree to the terms of use & privacy policy.</p>
+                    </div>
+                )}
+            </div>
         </div>
-        
-        <p className="loginsignup-login">
-          Already have an account ? <span>Login here</span>
-        </p>
-        <div className='loginsignup-agree'>
-          <input type='checkbox' name='' id=''/>
-          <p>By continuing, i agree to the terms of use & privacy policy.</p>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default LoginSignup
+export default LoginSignup;
